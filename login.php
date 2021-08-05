1c9f4a97e04b2de061e161033b1941b931258712
Pauleski <igorpauleski@hotmail.com>
2021-08-05 16:13:09 -0300

    Criado sistema de chaves de segurança e armazenamento na sessão

--- login.php
+++ login.php
@@ -10,7 +10,42 @@
 </head>
 <body>
 	<section id="main">
-		<form action="#" method="post" name="f_login" id="f_login">
+		<?php
+			if (isset($_POST["f_logar"])) {
+				$user=$_POST["f_user"];
+				$senha=$_POST["f_senha"];
+
+				//MySQL
+					//pesq. user, se existir;
+					//obter user e senha;
+					//comparar senha;
+
+
+				if (($user!="igor")or($senha!="123")) {
+					echo "<p id='lgErro'>Login incorreto!</p>";
+				} else {
+					//Criar a chave de segurança e armz. na sessão;
+					$chave1="abcdefghijklmnopqrstuvwxyz";
+					$chave2="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+					$chave3="0123456789";
+					$chave=str_shuffle($chave1.$chave2.$chave3);
+					$tam=strlen($chave);
+					$num="";
+					$qtde=rand(20,50);
+					for ($i=0; $i < $qtde; $i++) { 
+						$pos=rand(0,$tam);
+						$num.=substr($chave, $pos, 1);
+					}
+					session_start();
+					$_SESSION['numlogin']=$num;
+					$_SESSION['username']=$num;
+
+					//Fazer a chamada do gerenciamento;
+					header("location:gerenciamento.php?num=$num");
+				}
+			}
+		?>
+		<form action="login.php" method="post" name="f_login" id="f_login">
 			<label>Usuário</label><br>
 			<input type="text" name="f_user">
 			<br>
