2e0c35111e3ee6c9f3e4d6c23b2c23cc81fb5f54
Pauleski <igorpauleski@hotmail.com>
2021-08-08 00:31:01 -0300

    Iplementado a barra de gerecimento do sistema e configurado o visual do botôes

---
+++ gerenciamento.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<title>Document</title>
+</head>
+<body>
+
+	<section>
+		<p>Menu principal de gerenciamento.</p>
+	</section>
+
+	<nav>
+		<div class="menu_ger">
+			<button id="menua1" class="btmenu">Posts</button>
+			<div id="menub1" class="menub">
+				<a href="#" target="_self">Create</a>
+				<a href="#" target="_self">Read</a>
+				<a href="#" target="_self">Update</a>
+				<a href="#" target="_self">Delete</a>
+				<a href="#" target="_self">Index(Listagem)</a>
+			</div>
+		</div>
+
+		<div id="menua2" class="menu_ger">
+			<button class="btmenu">Users</button>
+			<div id="menub2" class="menub">
+				<a href="#" target="_self">Create</a>
+				<a href="#" target="_self">Update</a>
+				<a href="#" target="_self">Delete</a>
+			</div>
+		</div>
+
+		<div id="menua3" class="menu_ger">
+			<button class="btmenu">Logoff</button>
+			<div id="menub3" class="menub">
+				<a href="#" target="_self">Exit</a>
+			</div>
+		</div>
+	</nav>
+	
+</body>
+</html>
